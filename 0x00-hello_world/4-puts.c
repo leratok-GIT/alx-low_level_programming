5209c2466a16835306372e6dd4332bf4eb914192
lerato <alx@gmail.com>
2023-02-17 13:52:04 -0800

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - AC program that print aline with puts function
+ * Return: 0 (its correct is it shows the 0)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
