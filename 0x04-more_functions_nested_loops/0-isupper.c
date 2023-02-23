89aa293c414f1db632dfd2af0c3b0bfdcd978809
lerato <alx@gmail.com>
2023-02-23 10:55:09 -0800

    is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+# inclde "main.h"
+/**
+ * _isupper -checks for uppercase character
+ * @c: variables text
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
